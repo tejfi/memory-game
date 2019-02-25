388442e9f726919e40010802da6c559a0335460a
tejfi <adamfdeak@gmail.com>
2019-02-25 10:37:22 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/game')
+def game():
+    row_num=int(request.args.get('row-num'))
+    col_num=int(request.args.get('col-num'))
+    win_size=int(request.args.get('win-size'))
+    return render_template('game.html', row_num=row_num, col_num=col_num, win_size=win_size)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/game.js
@@ -0,0 +1,25 @@
+let card = document.querySelectorAll("card");
+let cards = [...card]
+
+let openedCards = [];
+
+var displayCard = function (){
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toggle("disabled");
+};
+
+
+// @description add opened cards to OpenedCards list and check if cards are match or not
+function cardOpen() {
+    openedCards.push(this);
+    var len = openedCards.length;
+    if(len === 2){
+        moveCounter();
+        if(openedCards[0].type === openedCards[1].type){
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+};
