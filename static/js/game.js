63af99c067eebc0759a029e85d6043d25de74007
vadnaireka <budaikreativ@gmail.com>
2019-02-26 15:09:37 +0100

    clickable cards + exem matching cards

--- static/js/game.js
+++ static/js/game.js
@@ -1,11 +1,24 @@
-const cardDeck = document.querySelectorAll("#card-deck");
-let hasFlippedCard = false;
-let firstCard. secondCard;
 
-function flipCard(){
-    this.classList.add(('flip');
-    if(!hasFlippedCard){
-        console.log('ye')
-    }
+let clickedCard = document.querySelectorAll(".card");
+let move_num = 0;
 
+for (let card of clickedCard){
+    card.addEventListener("click", function(event){
+        let target = event.currentTarget;
+        target.innerHTML = '<i class="fas fa-anchor"></i>';
+        target.classList.add("open");
+        move_num ++;
+        if (move_num ===2) {
+            const open_cards = document.querySelectorAll(".open");
+            if (open_cards[0].innerHTML === open_cards[1].innerHTML){
+                for (let card of open_cards){
+                    card.style.backgroundColor = 'red';
+                    card.classList.remove("open")
+                    card.classList.add("done")
+                }
+
+            }
+            move_num = 0;
+        }
+    });
 }
